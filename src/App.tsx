12709a82e33c2081ae5122d64854a7cef8d2f742
aristocrat44 <arcrt1993@gmail.com>
2024-03-18 17:10:10 +0545

    workflow deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div style={{ margin: "auto" }}>React + AWS S3 + Cloud Front</div>;
+  return (
+    <div style={{ margin: "auto" }}>
+      React + AWS S3 + Cloud Front with Github workflow deployment
+    </div>
+  );
 }
 
 export default App;
